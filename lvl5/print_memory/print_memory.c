d20b519a18586c44a374258f1b9d0f4f5bbd91e8
Sebastien GARCIA <sgarcia@e3r4p10.42.fr>
2018-04-12 16:41:58 +0200

    commit

--- lvl5/print_memory/print_memory.c
+++ lvl5/print_memory/print_memory.c
@@ -1,25 +1,19 @@
 #include <unistd.h>
-#include <stdio.h>
-
-void	ft_putchar(char c)
-{
-	write (1, &c, 1);
-}
 
 void	ft_putascii(unsigned char c)
 {
 	if (c > 31 && c < 127)
-		ft_putchar(c);
+		write (1, &c, 1);
 	else
-		ft_putchar('.');
+		write (1, ".", 1);
 }
 
 void	ft_puthex(unsigned char c)
 {
 	char	tab[16] = "0123456789abcdef";
 
-	ft_putchar(tab[c / 16]);
-	ft_putchar(tab[c % 16]);
+	write(1, &tab[c / 16], 1);
+	write(1, &tab[c % 16], 1);
 }
 
 void	print_line(unsigned char *str, size_t start, size_t max)
@@ -31,52 +25,39 @@ void	print_line(unsigned char *str, size_t start, size_t max)
 	{
 		ft_puthex(str[i]);
 		if (i % 2)
-			ft_putchar(' ');
+			write (1, " ", 1);
 		i++;
 	}
 	while (i < start + 16)
 	{
-		ft_putchar(' ');
-		ft_putchar(' ');
+		write (1, "  ", 2);
 		if (i % 2)
-			ft_putchar(' ');
+			write (1, " ", 1);
 		i++;
 	}
 	i = start;
 	while(i < start + 16 && i < max)
-	{
-		ft_putascii(str[i]);
-		i++;
-	}
-	ft_putchar('\n');
+		ft_putascii(str[i++]);
+	write (1, "\n", 1);
 }
 
 void	print_memory(const void *addr, size_t size)
 {
 	unsigned char	*str;
-	size_t	c;
-	int		i = 0;
+	size_t	start;
 
 	str = (unsigned char *)addr;
-	c = 0;
-	while (str[i])
+	start = 0;
+	while (start < size)
 	{
-		printf("||%c||", str[i]);
-		i++;
-	}
-	while (c < size)
-	{
-		print_line(str, c, size);
-		c += 16;
+		print_line(str, start, size);
+		start += 16;
 	}
 }
 
 int		main(void)
 {
-	int	tab[10] = {0, 23, 150, 255,
-	              12, 16,  42, 103};
-
+	int	tab[17] = {0, 23, 35560, 255, 12, 16,  42, 178903, 444687, 900445, 555467, 9999999, 6746516, 2646, 199999, 1};
 	print_memory(tab, sizeof(tab));
 	return (0);
 }
-
